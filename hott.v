b61a895b59a815f18f15326191bcba1a93aac19c
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-09-11 13:23:03 +0200

    -

--- hott.v
+++ hott.v
@@ -988,44 +988,44 @@ Set Printing Depth 100.
 Definition hott_3_8_5 : ∃ X (Y : X → Type), (∀ x, isSet (Y x))
   → notT ((Π (x : X), ∥(Y x)∥) → ∥(Π (x : X), Y x)∥)
 :=
-let X := {A : Type & ∥(ℬ = A)∥} in
-let x₀ := existT (λ A : Type, ∥(ℬ = A)∥) ℬ |(eq_refl ℬ)|:X in
-let Y := λ x : X, x₀ = x:Type in
-ex_intro _ X
-  (ex_intro _ Y
-     (λ _ _,
-      let (f, i) := equiv_eq_bool_trunc in
-      let (s, x) := i in
-      let (g, Hg) := s in
-      Σ_type2.hott_2_12_6
-        (hap
-           (f_equal
-              (λ e0 : ℬ ≃ ℬ,
-               let (x, i0) := e0 in
-               let (_, s7) := i0 in let (x0, _) := s7 in x0)
-              (eq_sym
-                 ((Hg
-                     (existT isequiv negb
-                        (existT (λ g0 : ℬ → bool, negb ◦ g0 ~~ id) negb
-                           (λ b : bool,
-                            if b as b0 return (negb (negb b0) = b0)
-                            then eq_refl true
-                            else eq_refl false),
-                        existT (λ h : ℬ → bool, h ◦ negb ~~ id) negb
-                          (λ b : bool,
-                           if b as b0 return (negb (negb b0) = b0)
-                           then eq_refl true
-                           else eq_refl false))))⁻¹ •
-                 (eq_sym
-                    ((Hg
-                        (existT isequiv id
-                           (existT (λ g0 : ℬ → bool, id ◦ g0 ~~ id) id
-                              (λ H : bool, eq_refl ((id ◦ id) H)),
-                            existT (λ h : ℬ → bool, h ◦ id ~~ id) id
-                              (λ H : bool, eq_refl ((id ◦ id) H)))))⁻¹ •
-               (ap f
-                  (isProp_isSet X isProp_pair_trunc x₀ x₀
-                      (g bool_eq_bool_id) (g bool_eq_bool_negb))))))))
-      false))).
+  let X := {A : Type & ∥(ℬ = A)∥} in
+  let x₀ := existT (λ A : Type, ∥(ℬ = A)∥) ℬ |(eq_refl ℬ)|:X in
+  let Y := λ x : X, x₀ = x:Type in
+  ex_intro _ X
+    (ex_intro _ Y
+       (λ _ _,
+        let (f, i) := equiv_eq_bool_trunc in
+        let (s, x) := i in
+        let (g, Hg) := s in
+        Σ_type2.hott_2_12_6
+          (hap
+             (f_equal
+                (λ e0 : ℬ ≃ ℬ,
+                 let (x, i0) := e0 in
+                 let (_, s7) := i0 in let (x0, _) := s7 in x0)
+                (eq_sym
+                   ((Hg
+                       (existT isequiv negb
+                          (existT (λ g0 : ℬ → ℬ, negb ◦ g0 ~~ id) negb
+                             (λ b : ℬ,
+                              if b as b0 return (negb (negb b0) = b0)
+                              then eq_refl true
+                              else eq_refl false),
+                          existT (λ h : ℬ → ℬ, h ◦ negb ~~ id) negb
+                            (λ b : ℬ,
+                             if b as b0 return (negb (negb b0) = b0)
+                             then eq_refl true
+                             else eq_refl false))))⁻¹ •
+                   (eq_sym
+                      ((Hg
+                          (existT isequiv id
+                             (existT (λ g0 : ℬ → ℬ, id ◦ g0 ~~ id) id
+                                (λ H : ℬ, eq_refl ((id ◦ id) H)),
+                              existT (λ h : ℬ → ℬ, h ◦ id ~~ id) id
+                                (λ H : ℬ, eq_refl ((id ◦ id) H)))))⁻¹ •
+                 (ap f
+                    (isProp_isSet X isProp_pair_trunc x₀ x₀
+                        (g bool_eq_bool_id) (g bool_eq_bool_negb))))))))
+        false))).
 
 bbb.
