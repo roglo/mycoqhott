d5872e18ef459414f92dcecb13c494c598703386
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-09-11 17:34:20 +0200

    -

--- hott.v
+++ hott.v
@@ -957,11 +957,7 @@ set (X := Σ (B : Type), ∥(A = B)∥).
 set (x₀ := existT _ A |(eq_refl A)|:X); simpl in x₀.
 transitivity x₀; subst x₀.
  symmetry.
- destruct x as (C, p).
-pose proof PT_elim p as q.
-bbb.
-
-; apply (Σ_type.pair_eq (PT_elim p)), PT_eq.
+ destruct x as (C, p); apply (Σ_type.pair_eq (PT_elim p)), PT_eq.
 
  destruct y as (C, p); apply (Σ_type.pair_eq (PT_elim p)), PT_eq.
 Defined.
