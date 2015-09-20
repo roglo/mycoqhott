21f36957449830ee96236d5916d6533c27a28980
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-09-20 05:12:38 +0200

    ex 3.12 completed

--- chap3.v
+++ chap3.v
@@ -1566,26 +1566,26 @@ PT_elim =
     ∥(∥A∥ → A)∥. (This property is a very simple form of the axiom of
     choice, which can fail in the absence of LEM; see [KECA13].)" *)
 
+(* very strange proof; in the case when A is not a mere proposition,
+   we artifially create an element of A in the following steps:
+   - using LEM to save the opposite of the goal in the hypotheses, as H
+   - the new goal is then the contradiction ⊥
+   - putting isProp A as new goal by applying notT (isProp A)
+   - introducing then x et y as elements of A → got it! actually two: x and y
+   - setting it is a contradiction (exfalso)
+   - putting H back as goal
+   When we have a value x of type A, the proof is done by PT_intro
+   and intros. *)
+
 Definition ex_3_12 : LEM → ∀ A, ∥(∥A∥ → A)∥.
 Proof.
 intros lem A.
 pose proof hott_3_3_5_i A as PPA.
 pose proof lem (isProp A) PPA as H.
 destruct H as [PA| NPA]; [ apply PT_intro, PT_elim, PA; assumption | ].
-bbb.
-
-Definition toto {A} : LEM → notT (isProp A) → A.
-Proof.
-intros lem NPA.
-Print LEM.
-bbb.
-
-Check (PT_rec A (isProp A)).
-unfold notT in NPA.
-apply PT_intro; intros a.
-unfold LEM in lem.
-Print PT_rec.
-assert (isProp (notT (isProp A))) by (apply isPropNot, PPA).
-pose proof (PT_rec A (not (isProp A)) (λ _, NPA) H).
-destruct H0 as (g, H1).
-bbb.
+pose proof lem _ (@PT_eq (∥A∥ → A)) as H.
+destruct H as [H| H]; [ apply H | exfalso ].
+apply NPA; clear NPA; intros x y.
+exfalso; apply H.
+apply PT_intro; intros; apply x.
+Defined.
