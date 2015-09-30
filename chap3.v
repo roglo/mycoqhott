7d617b095e5e2b20ae08ad41eb09bcc486bdabb8
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-09-30 10:24:12 +0200

    -

--- chap3.v
+++ chap3.v
@@ -2224,40 +2224,23 @@ intros a b p q.
 apply le_unique.
 Defined.
 
-Definition my_le_0_r_tac : ∀ a, a ≤ 0 → a = 0.
+Definition my_le_0_r : ∀ a, a ≤ 0 → a = 0.
 Proof.
 intros a ale.
 inversion ale; apply eq_refl.
 Defined.
 
-Print eq_sym.
-
-Definition my_le_0_r : ∀ a, a ≤ 0 → a = 0 :=
-  λ a ale,
-  match ale in (_ ≤ n) return (n = 0 → a = 0) with
-  | le_n _ =>
-      λ H,
-      match
-        match H in (_ = b) return (b = a) with eq_refl _ => eq_refl a end
-      with
-      | eq_refl _ => eq_refl 0
-      end
-  | le_S _ m lem =>
-      λ H,
-      match
-        match H in (_ = y) return match y with 0 => ⊥ | S _ => ⊤ end with
-        | eq_refl _ => I
-        end
-      with end lem
-  end (eq_refl 0).
+Definition my_nle_succ_0 : ∀ a, not (S a ≤ 0).
+Proof.
+intros a ale.
+inversion ale.
+Defined.
 
 Definition my_le_S_n : ∀ a b, S a ≤ S b → a = b.
 Proof.
 intros a b p.
-revert a p.
-induction b; intros.
- induction a; [ apply eq_refl | ].
-Abort.
+inversion p as [q| c q r]; [ apply eq_refl | ].
+bbb.
 
 Definition my_lt_1_r : ∀ a, a < 1 → a = 0.
 Proof.
