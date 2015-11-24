1186b357cfa14c3920ea82d150ab01054e93dc12
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2015-11-24 13:32:49 +0100

    -

--- chap5.v
+++ chap5.v
@@ -20,3 +20,30 @@ Notation "f '~~' g" := (dt_homotopy f g) (at level 70).
 Open Scope nat_scope.
 
 (* "Chapter 5 - Induction" *)
+
+(* "5.1 Introduction to inductive types" *)
+
+(* "Theorem 5.1.1. Let f, g : Π (x:ℕ) E(x) be two functions
+    which satisfy the recurrences
+         e_z : E(0)     and     e_s : Π (n : ℕ) E(n) → E (succ(n))
+    up to propositional equality, i.e., such that
+         f(0) = e_z     and     g(0) = e z
+    as well as
+         Π (n : ℕ) f(succ(n)) = e_s(n, f(n)),
+         Π (n : ℕ) g(succ(n)) = e_s(n, g(n)),
+    Then f and g are equal." *)
+
+Definition hott_5_1_1 E (f g : Π (x : ℕ), E x)
+  (e_z : E 0) (e_s : Π (n : ℕ), E n → E (S n)) :
+  f 0 = e_z → g 0 = e_z
+  → (Π (n : ℕ), f (S n) = e_s n (f n))
+  → (Π (n : ℕ), g (S n) = e_s n (g n))
+  → f = g.
+Proof.
+intros fz gz fs gs.
+apply Π_type.funext; intros n.
+induction n; [ destruct fz, gz; apply eq_refl | ].
+pose proof (fs n) as p.
+pose proof (gs n) as q.
+destruct IHn, p, q; apply eq_refl.
+Defined.
