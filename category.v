198d6e9cf8af6607b13b051fbbe53ce4e9818d5e
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-06-30 23:52:13 +0200

    -

--- category.v
+++ category.v
@@ -657,8 +657,36 @@ Definition functor_cCoCone2_of_cCoCone {J C} {D : functor J C} :
      f_comp_prop _ _ _ _ _ := eq_refl;
      f_id_prop _ := eq_refl |}.
 
+Definition co_cone_obj_of_cone_fop_obj {J C} {D : functor J C} :
+    Obj (cCoCone2 (fop D)) → Obj (cCoCone D) :=
+  λ cn,
+  let J' := op J in
+  let C' := op C in
+  let D' := fop D in
+  {| c_top := c_top D' cn : Obj (cCoCone D) |}.
+  {| c_top := c_top D' cn : co_cone D |}.
+
+...
+
+  {| Obj := co_cone D;
+  {| Obj := cone D_op;
+
+...
+
+(*
+Definition co_cone_obj_of_cone_fop_obj {J C} {D : functor J C} :
+    Obj (cCoCone2 (fop D)) → Obj (cCoCone D) :=
+  λ cn,
+  let J' := op J in
+  let C' := op C in
+  let D' := fop D in
+  {| c_top := c_top D' cn : Obj C';
+     c_fam j := c_fam D' cn j : @Hom C' (c_top D' cn) (@f_map_obj _ _ D' j);
+     c_commute i j := c_commute D' cn i j |}.
+*)
+
 Definition functor_cCoCone_of_cCoCone2 {J C} {D : functor J C} :
   functor (cCoCone2 (fop D)) (cCoCone D) :=
-  {| f_map_obj := 42 |}.
+  {| f_map_obj := co_cone_obj_of_cone_fop_obj |}.
 
 ...
