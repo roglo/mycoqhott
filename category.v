99281a985b705ee931fe2937396d9dca01d86309
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-07-02 11:49:06 +0200

    -

--- category.v
+++ category.v
@@ -397,6 +397,24 @@ Definition are_isomorphic_categories_2 (C D : category) :=
           ∃ T : Hom x y → Hom (f_map_obj G (f_map_obj F x)) (f_map_obj G (f_map_obj F y)),
           ∀ f : Hom x y, f_map_arr G (f_map_arr F f) = T f } } } }.
 
+Definition transport_pair {A} B C x y (p : x = y) b c
+  : hott4cat.transport (λ z : A, (B z * C z)%type) p (b, c) =
+    (hott4cat.transport B p b, hott4cat.transport C p c)
+  := match p with
+     | eq_refl _ =>
+         eq_refl (hott4cat.transport B (eq_refl x) b, hott4cat.transport C (eq_refl x) c)
+     end.
+
+(*
+Theorem glop A B : ∀ (x y : A) (z t : B) (p : x = y) (q : z = t), (x, z) = (y, t).
+Proof.
+intros.
+now destruct p, q.
+Defined.
+Search ((_, _) = (_, _)).
+...
+*)
+
 Example glop (C D : category) :
   ∀ (F : functor C D) (G : functor D C)
      (GF : ∀ x : Obj C, f_map_obj G (f_map_obj F x) = x)
@@ -406,6 +424,14 @@ Example glop (C D : category) :
   ∀ f : Hom x y, f_map_arr G (f_map_arr F f) = T f.
 Proof.
 intros.
+specialize (@transport_pair (Obj C)) as H1.
+...
+specialize (@hott4cat.transport (Obj C * Obj C)) as H1.
+specialize (H1 (λ z, let '(x, y) := z in Hom x y)).
+specialize (H1 (x, y)); cbn in H1.
+specialize (H1 (f_map_obj G (f_map_obj F x), f_map_obj G (f_map_obj F y))).
+cbn in H1.
+...
 specialize (@hott4cat.transport (Obj C)) as H1.
 specialize (H1 (λ x, Hom x (f_map_obj G (f_map_obj F y)))).
 specialize (H1 x (f_map_obj G (f_map_obj F x)) (eq_sym (GF x))).
