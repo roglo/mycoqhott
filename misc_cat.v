9fd8333cc0eceb56b0e3bec2eec489a2015b4d93
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-08-05 17:44:35 +0200

    -

--- misc_cat.v
+++ misc_cat.v
@@ -309,6 +309,11 @@ exists eq_refl; cbn.
 apply Hom_set.
 Defined.
 
+Theorem coslice_slice_id_prop {C} {A : Ob C} (X : Ob (CosliceCat A)) :
+  @id (@Hom (@CosliceCat C A) X X) (@idc (@CosliceCat C A) X) = @idc (@SliceCat C⁰ A)⁰ X.
+Proof.
+...
+
 Theorem coslice_slice {C} (A : Ob C) :
   are_equivalent_categories (CosliceCat A) (@SliceCat C⁰ A)⁰.
 Proof.
@@ -319,7 +324,8 @@ assert (F : functor (CosliceCat A) (@SliceCat C⁰ A)⁰). {
          X : Ob (@SliceCat C⁰ A)⁰;
        f_hom X Y (f : @Hom (@CosliceCat C A) X Y) :=
          id f : @Hom (@SliceCat C⁰ A)⁰ X Y;
-       f_comp_prop _ _ _ := coslice_slice_comp_prop |}.
+       f_comp_prop _ _ _ := coslice_slice_comp_prop;
+       f_id_prop := coslice_slice_id_prop |}.
 ...
 
 (*  The category Sets∗ of pointed sets consists of sets A with a distinguished
