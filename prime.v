b1453482a13ecf6d7c7387968507c340b413918d
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-09-07 19:20:49 +0200

    -

--- prime.v
+++ prime.v
@@ -25,17 +25,36 @@ Proof. easy. Qed.
 
 Theorem not_prime_div : ∀ n d, 2 ≤ n → d < n →
   prime_test n d = false
-  → ∃ m, 1 < m ∧ Nat.divide m n.
+  → ∃ m, is_prime m = true ∧ Nat.divide m n.
 Proof.
 intros * Hn Hd Hp.
 revert n Hn Hd Hp.
 induction d; intros; [ easy | ].
 cbn in Hp.
 rewrite fold_mod_succ in Hp.
-...
+destruct d; [ easy | ].
+remember (n mod (S (S d))) as m eqn:Hm.
+symmetry in Hm.
+destruct m. {
+  clear Hp.
+  apply Nat.mod_divide in Hm; [ | easy ].
+  remember (is_prime (S (S d))) as b eqn:Hb.
+  symmetry in Hb.
+  destruct b; [ now exists (S (S d)) | ].
+  unfold is_prime in Hb.
+  apply IHd in Hb; [ | flia | flia ].
+  destruct Hb as (m & Hpm & Hmd).
+  exists m.
+  split; [ easy | ].
+  now apply (Nat.divide_trans _ (S (S d))).
+}
+apply IHd in Hp; [ | easy | flia Hd ].
+destruct Hp as (p & Hp & Hpn).
+now exists p.
+Qed.
 
 Theorem not_prime : ∀ n, 2 ≤ n →
-  is_prime n = false → ∃ d, 1 < d ∧ Nat.divide d n.
+  is_prime n = false → ∃ d, is_prime d = true ∧ Nat.divide d n.
 Proof.
 intros * Hn Hp.
 destruct n; [ flia Hn | ].
@@ -43,7 +62,7 @@ destruct n; [ flia Hn | ].
 clear Hn.
 unfold is_prime in Hp.
 apply (not_prime_div _ (S n)); [ flia | flia | easy ].
-...
+Qed.
 
 Theorem infinite_primes : ∀ n, ∃ m, m > n ∧ is_prime m = true.
 Proof.
@@ -69,4 +88,18 @@ destruct pfn.
  specialize (fact_neq_0 n) as H.
  remember (fact n) as m eqn:Hm.
  destruct m; [ easy | flia ].
--idtac.
+-destruct n; [ now subst fn | ].
+ apply not_prime in Hpfn. 2: {
+   clear Hpfn; subst fn; cbn.
+   specialize (fact_neq_0 n) as H1.
+   flia H1.
+ }
+ destruct Hpfn as (d & Hd & Hdn).
+ exists d.
+ split; [ | easy ].
+ destruct Hdn as (z & Hz).
+ subst fn.
+ destruct z; [ flia Hz | ].
+ apply (Nat.mul_lt_mono_pos_l (S z)); [ flia | ].
+ rewrite <- Hz.
+...
