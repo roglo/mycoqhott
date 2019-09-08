70669ac3775a33534204f934cf1d59723708d0ea
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2019-09-08 12:05:47 +0200

    -

--- prime.v
+++ prime.v
@@ -119,27 +119,23 @@ revert n Hdn.
 induction d; intros; [ easy | ].
 destruct n; [ flia Hdn | ].
 apply Nat.succ_le_mono in Hdn.
-apply IHd in Hdn.
-apply Nat.mod_divide in Hdn; [ | apply fact_neq_0 ].
-destruct Hdn as (c, Hc).
-rewrite Nat_fact_succ, Hc.
+specialize (IHd _ Hdn) as H1.
+apply Nat.mod_divide in H1; [ | apply fact_neq_0 ].
 apply Nat.mod_divide; [ apply fact_neq_0 | ].
 unfold Nat.divide.
-...
+destruct H1 as (c, Hc).
+rewrite Nat_fact_succ.
 exists (S n * (c / S d)).
 rewrite <- Nat.mul_assoc; f_equal.
 rewrite Nat_fact_succ.
 rewrite Nat.mul_assoc; f_equal.
+assert (Nat.gcd c (S d) = S d). {
 ...
-assert (H : Nat.divide (S d) c). {
-  exists (fact n / fact (d + 1)).
-  rewrite Nat.add_1_r, Nat_fact_succ.
-  rewrite Hc.
-
 }
-destruct H as (z, Hz).
-rewrite Hz at 2.
-now rewrite Nat.div_mul.
+rewrite <- H at 1.
+rewrite Nat.gcd_div_swap.
+rewrite H, Nat.div_same; [ | easy ].
+now rewrite Nat.mul_1_r.
 ...
 
 Theorem Nat_fact_mul_div : ∀ n d,
